d66a63411267e7405917f30a8f7882f11cfbe731
David Streid <david.streid.nyc@gmail.com>
2019-08-19 10:57:57 -0400

    Adding gtz to script; Refactoring

--- compression_test.sh
+++ compression_test.sh
@@ -1,16 +1,39 @@
 COMPRESSION_ALGOS=( gzip bzip2 pbzip2 xz pigz )
 
+compress(){
+  algo=$1
+  root=$2
+  file=$3
+
+  echo "Algo: $algo"
+  compressed_file="./compressed/${root##*/}.$algo"
+  { time cat $file | eval "$algo -9" > $compressed_file; } 2>&1
+  du $compressed_file
+}
+
+compress_gtz(){
+  path_to_gtz=$1
+  root=$2
+  file=$3
+
+  algo="gtz"
+  echo "Algo: $algo"
+  compressed_file="./compressed/${root##*/}.$algo"
+  { time eval "$path_to_gtz $file -o $compressed_file 2>/dev/null"; } 2>&1
+  du $compressed_file
+}
+
 for file in ./fastq/*.fastq; 
 do
 	eval "du $file"
 	root=${file%.fastq}
 	for algo in "${COMPRESSION_ALGOS[@]}"
 	do
-		echo "Algo: $algo"   
-		compressed_file="./compressed/${root##*/}.$algo"
-		{ time cat $file | eval "$algo -9" > $compressed_file; } 2>&1
-		eval "du $compressed_file"
+		compress $algo $root $file
 	done
+
+  path_to_gtz="~/.config/GTZ/gtz"   # Modify this if not using default installation
+  compress_gtz $path_to_gtz $root $file
 done
 
 
